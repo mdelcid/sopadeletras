d4fcd9adb661d22727084f52b20f4fca7d446772
Your Name <you@example.com>
2014-03-24 09:47:44 -0600

    sopa

---
+++ sopa/src/sopa/Sopa.java
@@ -0,0 +1,209 @@
+package sopa;
+
+
+import java.util.Scanner;
+
+public class Sopa 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.print("\n");
+		System.out.print("------------Sopa de Letras©------------");
+		System.out.print("\n lvl 1 ");
+		System.out.print("\n Vidas [♥] [♥] [♥]");
+		char sopa[][]=new char[5][5];
+		Scanner lol=new Scanner(System.in);
+		sopa[0][0]='c';
+		sopa[1][0]='o';
+		sopa[2][0]='l';
+		sopa[3][0]='o';
+		sopa[4][0]='l';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][0]+"] ");
+		System.out.print(" ["+sopa[2][0]+"] ");
+		System.out.print(" ["+sopa[3][0]+"] ");
+		System.out.print(" ["+sopa[4][0]+"] ");
+		sopa[0][0]='q';
+		sopa[1][1]='r';
+		sopa[2][2]='p';
+		sopa[3][3]='h';
+		sopa[4][4]='a';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='o';
+		sopa[1][1]='f';
+		sopa[2][2]='p';
+		sopa[3][3]='f';
+		sopa[4][4]='p';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='s';
+		sopa[1][1]='a';
+		sopa[2][2]='p';
+		sopa[3][3]='i';
+		sopa[4][4]='i';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='o';
+		sopa[1][1]='a';
+		sopa[2][2]='p';
+		sopa[3][3]='i';
+		sopa[4][4]='z';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		System.out.print("\n-------------------------------------------");
+		System.out.println("\n Ingrese las Palabras Ocultas ");
+		for (int i=0;i<2;i ++){
+		String str=lol.next();
+		str=(" lapiz"+"Oso");
+		System.out.println(" Correcto ");
+		}
+		System.out.print("\n-------------Felicidades Avanzas al Level2------------- ");
+		System.out.print("\n");
+		System.out.print("\n");
+		System.out.print("\n");
+
+		System.out.print("\n------------Sopa de Letras©------------");
+		System.out.print("\n lvl 2 ");
+		System.out.print("\n Vidas [♥] [♥] [♥]");
+
+		sopa[0][0]='c';
+		sopa[1][0]='o';
+		sopa[2][0]='l';
+		sopa[3][0]='o';
+		sopa[4][0]='r';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][0]+"] ");
+		System.out.print(" ["+sopa[2][0]+"] ");
+		System.out.print(" ["+sopa[3][0]+"] ");
+		System.out.print(" ["+sopa[4][0]+"] ");
+		sopa[0][0]='q';
+		sopa[1][1]='r';
+		sopa[2][2]='p';
+		sopa[3][3]='h';
+		sopa[4][4]='a';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='o';
+		sopa[1][1]='f';
+		sopa[2][2]='p';
+		sopa[3][3]='f';
+		sopa[4][4]='p';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='s';
+		sopa[1][1]='a';
+		sopa[2][2]='p';
+		sopa[3][3]='i';
+		sopa[4][4]='i';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='o';
+		sopa[1][1]='a';
+		sopa[2][2]='p';
+		sopa[3][3]='i';
+		sopa[4][4]='z';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		System.out.print("\n-------------------------------------------");
+		System.out.println("\n Ingrese las Palabras Ocultas ");
+		for (int i=0;i<3;i ++){
+		String str=lol.next();
+		str=(" lapiz"+"Oso"+"Color");
+		System.out.println(" Correcto ");
+		}
+		System.out.print("\n-------------Felicidades Avanzas al Level3------------- ");
+		System.out.print("\n");
+		System.out.print("\n");
+		System.out.print("\n");
+		System.out.print("------------Sopa de Letras©------------");
+		System.out.print("\n lvl 3 ");
+		System.out.print("\n Vidas [♥] [♥] [♥]");
+		sopa[0][0]='c';
+		sopa[1][0]='o';
+		sopa[2][0]='l';
+		sopa[3][0]='o';
+		sopa[4][0]='r';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][0]+"] ");
+		System.out.print(" ["+sopa[2][0]+"] ");
+		System.out.print(" ["+sopa[3][0]+"] ");
+		System.out.print(" ["+sopa[4][0]+"] ");
+		sopa[0][0]='q';
+		sopa[1][1]='r';
+		sopa[2][2]='p';
+		sopa[3][3]='h';
+		sopa[4][4]='a';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='o';
+		sopa[1][1]='f';
+		sopa[2][2]='p';
+		sopa[3][3]='f';
+		sopa[4][4]='m';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='s';
+		sopa[1][1]='a';
+		sopa[2][2]='p';
+		sopa[3][3]='i';
+		sopa[4][4]='o';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		sopa[0][0]='o';
+		sopa[1][1]='a';
+		sopa[2][2]='p';
+		sopa[3][3]='i';
+		sopa[4][4]='z';
+		System.out.print("\n ["+sopa[0][0]+"] ");
+		System.out.print(" ["+sopa[1][1]+"] ");
+		System.out.print(" ["+sopa[2][2]+"] ");
+		System.out.print(" ["+sopa[3][3]+"] ");
+		System.out.print(" ["+sopa[4][4]+"] ");
+		System.out.print("\n-------------------------------------------");
+		System.out.println("\n Ingrese las Palabras Ocultas ");
+		for (int i=0;i<4;i ++){
+		String str=lol.next();
+		str=(" lapiz"+"Oso"+"Color"+"Ramo");
+		System.out.println(" Correcto ");
+		}
+		System.out.print("\n-------------Felicidades Ganaste Trofeo------------- ");
+		System.out.print("\n ");
+		}
+
+		}
